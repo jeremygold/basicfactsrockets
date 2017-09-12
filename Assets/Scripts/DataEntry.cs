a2b662a8ea38d112ae81d5ce7f9254dc835da296
Jeremy Gold <jeremy.gold@ikegps.com>
2017-09-12 20:01:02 +1200

    Initial creation

---
+++ Assets/Scripts/DataEntry.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataEntry : MonoBehaviour {
+	public GUIText  gt;
+
+	void Start()
+	{
+		gt = GetComponent<GUIText>();
+		gt.text = "";
+	}
+
+	void Update()
+	{
+		foreach (char c in Input.inputString) {
+			if (c == '\b') { // has backspace/delete been pressed?
+				if (gt.text.Length != 0) {
+					gt.text = gt.text.Substring (0, gt.text.Length - 1);
+				}
+			} else if ((c == '\n') || (c == '\r')) { // enter/return
+				print ("User entered their name: " + gt.text);
+				bool hit = false;
+				if (gt.text == "42") {
+					hit = true;
+				}
+				gt.text = "";
+
+				GunControl gunScript = GameObject.Find("Main Camera").GetComponent<GunControl>();
+				gunScript.RequestFire (hit);
+
+			} else {
+				gt.text += c;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GunControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform shotPos;
+	public float shotForce=1000f;
+	public Transform hitTarget;
+	public Transform missTarget;
+
+	private bool firePending = false;
+	private bool onTarget = false;
+
+	void Update () {
+		if (firePending) {
+			Fire ();
+			firePending = false;
+		}
+	}
+	void Fire() {
+		Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+		if (onTarget) {
+			transform.LookAt (hitTarget);
+			shot.transform.LookAt (hitTarget);
+		} else {
+			transform.LookAt (missTarget);
+			shot.transform.LookAt (missTarget);
+		}
+			
+		for (int i = 0; i < 50; i++) {
+			shot.AddForce (shot.transform.forward * shotForce);
+		}
+	}
+
+	public void RequestFire(bool _onTarget) {
+		firePending = true;
+		onTarget = _onTarget;
+	}
+}
